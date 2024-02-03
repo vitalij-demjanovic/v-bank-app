97bd3cb08a967b38c0ff953534eaa4d535f337e1
vitalij_demjanovic <vitalij.demjanovic@maisondidee.com>
2024-02-03 22:54:35 +0100

    UPDATE functionality logout

--- app/components/layout/Header.tsx
+++ app/components/layout/Header.tsx
@@ -1,11 +1,23 @@
+'use client';
+
 import Cog8ToothIcon from '@heroicons/react/24/outline/Cog8ToothIcon';
 import Bell from '@heroicons/react/24/outline/BellIcon';
 import Chat from '@heroicons/react/24/outline/ChatBubbleOvalLeftEllipsisIcon';
 import UserIcon from '@heroicons/react/24/outline/UserIcon';
 import MagnifyingGlassIcon from '@heroicons/react/24/outline/MagnifyingGlassIcon';
+import LogOut from '@heroicons/react/24/outline/ArrowLeftStartOnRectangleIcon';
 import Link from 'next/link';
+import { useCookies } from 'react-cookie';
+import { useRouter } from 'next/navigation';
 
 export default function Header() {
+  const [cookie, setCookie, removeCookie] = useCookies(['token']);
+  const router = useRouter();
+  const logOut = () => {
+    removeCookie('token');
+    router.push('/authentication');
+  };
+
   return (
     <header className='flex justify-between bg-light-grey px-[32px] py-[25px] text-dark-grey'>
       <div>
@@ -22,19 +34,22 @@ export default function Header() {
       </div>
       <div>
         <ul className='flex items-center'>
-          <li className='mr-5 rounded-full border border-dark-grey p-[8px]'>
+          <li className='mr-5 cursor-pointer rounded-full border border-dark-grey p-[8px] transition duration-300 ease-in-out hover:border-primary hover:bg-primary hover:text-white'>
             <Chat className='h-[28px] w-[28px]' />
           </li>
-          <li className='mr-5 rounded-full border border-dark-grey p-[8px]'>
+          <li className='mr-5 cursor-pointer rounded-full border border-dark-grey p-[8px] transition duration-300 ease-in-out hover:border-primary hover:bg-primary hover:text-white'>
             <Bell className='h-[28px] w-[28px]' />
           </li>
-          <li className='mr-5 rounded-full border border-dark-grey p-[8px]'>
+          <li className='mr-5 cursor-pointer rounded-full border border-dark-grey p-[8px] transition duration-300 ease-in-out hover:border-primary hover:bg-primary hover:text-white'>
             <Cog8ToothIcon className='h-[28px] w-[28px]' />
           </li>
-          <li>
+          <li className='mr-5 cursor-pointer rounded-full border border-dark-grey p-[8px] transition duration-300 ease-in-out hover:border-primary hover:bg-primary hover:text-white'>
             <Link href={'/user'}>
               <UserIcon className='h-[28px] w-[28px]' />
             </Link>
+          </li>
+          <li className='cursor-pointer rounded-full border border-dark-grey p-[8px] transition duration-300 ease-in-out hover:border-primary hover:bg-primary hover:text-white'>
+            <LogOut className='h-[28px] w-[28px] rotate-180' onClick={logOut} />
           </li>
         </ul>
       </div>
